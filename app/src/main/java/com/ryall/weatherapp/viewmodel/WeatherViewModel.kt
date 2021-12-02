fcdd7f4d9f6e8bdd7f1a764c2c9285528cac7033
RyallNovember <ryallitz@gmail.com>
2021-12-02 14:02:22 +0200

    removed imports

--- app/src/main/java/com/ryall/weatherapp/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/ryall/weatherapp/viewmodel/WeatherViewModel.kt
@@ -1,17 +1,12 @@
 package com.ryall.weatherapp.viewmodel
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.Network
-import android.net.NetworkCapabilities.*
-import android.net.NetworkInfo
-import android.os.Build
 import android.util.Log
-import androidx.lifecycle.*
-import com.ryall.weatherapp.MyApplication
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ryall.weatherapp.models.WeatherResponse
 import com.ryall.weatherapp.repository.WeatherRepository
-import dagger.hilt.android.internal.Contexts.getApplication
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -24,13 +19,13 @@ constructor(private val repository: WeatherRepository) : ViewModel() {
     val weatherResponse: LiveData<WeatherResponse>
         get() = _resp
 
-      fun getWeather(lat:Double, lon:Double) = viewModelScope.launch {
-              repository.getWeather(lat,lon).let { response ->
-                  if (response.isSuccessful){
-                      _resp.postValue(response.body())
-                  }else{
-                      Log.d("Tag", "getWeather Error Response: ${response.message()}")
-                  }
-              }
-          }
+    fun getWeather(lat: Double, lon: Double) = viewModelScope.launch {
+        repository.getWeather(lat, lon).let { response ->
+            if (response.isSuccessful) {
+                _resp.postValue(response.body())
+            } else {
+                Log.d("Tag", "getWeather Error Response: ${response.message()}")
+            }
+        }
     }
+}
